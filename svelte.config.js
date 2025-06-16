3817717f3e85c9b9c09e82b5a83ee81f4bcf8684
Trang Ha <trang.haphamdiem@gmail.com>
2025-06-16 21:54:14 +0700

    fix(patch): build in vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,28 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: false
-		})
-	}
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs18.x', // or 'nodejs22.x' depending on your environment
+    }),
+  },
 };
 
-export default config; 
+export default config;
+
+// import adapter from '@sveltejs/adapter-static';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter({
+// 			pages: 'build',
+// 			assets: 'build',
+// 			fallback: 'index.html',
+// 			precompress: false,
+// 			strict: false
+// 		})
+// 	}
+// };
+
+// export default config;
